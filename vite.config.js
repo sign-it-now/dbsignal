30671e177b7c8f1aea11e872ac72af26f6a87424
sign-it-now <258903863+sign-it-now@users.noreply.github.com>
2026-04-07 16:12:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
-import { defineConfig } from ‘vite’
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-build: {
-outDir: ‘dist’,
-rollupOptions: {
-input: ‘index.html’,
-},
-},
-optimizeDeps: {
-include: [
-‘@walletconnect/sign-client’,
-‘@reown/appkit’,
-‘@hashgraph/hedera-wallet-connect’,
-],
-},
-define: {
-global: ‘globalThis’,
-},
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: 'index.html',
+    },
+  },
+  define: {
+    global: 'globalThis',
+  },
 })
